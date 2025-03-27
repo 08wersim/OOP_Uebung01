5b85d38a5553ce5d7a24dee106c8319b84572cdc
Simon <08wersim@rgtfo-me.it>
2025-03-27 08:46:48 +0100

    Hinzufügen von analogblink.h /.cpp und helper.h /.cpp

--- src/analogblinker.h
+++ src/analogblinker.h
@@ -4,6 +4,24 @@
 
 #include <Arduino.h>
 
+class analogblinker
+{
 
+public: 
+    bool sync = false, enable = false;
+    uint8_t step = 0, polltime = 0;
+    
+    // PinNr1, PinNr2, Blinkzeit[ms], Abfrage-Zeit[ms], Sync-Modus, Enable[T/F]
+    void init(uint8_t, uint8_t, uint16_t, uint8_t, bool, bool);
+    void poll();
+    void setblinktime(uint16_t);
+
+private:
+    unsigned long lastpoll = 0;
+    uint8_t pin1 = 0, pin2 = 0;
+    uint16_t dutycycle = 0;
+    uint16_t blinkTime = 0;
+
+};
 
 #endif
--- src/helper.h
+++ src/helper.h
@@ -2,6 +2,42 @@
 #define HELPER_H
 
 #include <Arduino.h>
+#include "analogblinker.h"
+
+class button
+{
+public:
+
+    // Member-Variable(n)
+    bool rising = false;
+    bool falling = false;
+    bool longpress = false;
+
+    // Methode(n)
+    // Pin-Nummer, true = negative Logik, Debounce-Zeit [ms], Zeitgrenze für langen Druck [ms]
+    void init(uint8_t, bool, uint8_t, uint16_t);
+
+    void poll();
+
+private:
+
+    bool db_last = false;
+    unsigned long lasttime = 0;
+    unsigned long last_rising = 0;
+    bool last = false;
+    bool in = false;
+    bool neg = false;
+    bool debounced = false;
+
+    uint8_t pinNr = 0;
+    uint8_t debounce_time = 20;
+    uint16_t longpress_time = 1000;
+
+    // wenn Debounce-Zeit = 0, dann kein Debounce
+    void debounce();
+    void check_edge();
+    void check_longpress();
+};
 
 
 #endif
