841f40db8df105fa9b717b87e52b30dbc0ef8429
Martin G <08germar@rgtfo-me.it>
2025-03-27 09:13:58 +0100

    digitalblinker header

--- src/digitalblinker.h
+++ src/digitalblinker.h
@@ -2,6 +2,23 @@
 #define DIGITALBLINKER_H
 
 #include <Arduino.h>
+class blinker
+{
+public:
+
+bool enable;
+void init(uint8_t, uint8_t, bool, bool, bool);
+void poll();
+
+private:
+
+unsigned long lastblink;
+bool inv1 = false, inv2 = false;
+uint8_t pin1, pin2;
+uint16_t blinktime;
+uint8_t convertDvToBt(uint16_t);
+
+};
 
 
 #endif
